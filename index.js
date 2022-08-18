4c86e7dfa2aa2d6ead3ecfa0cac0831c3843ae19
Justin Osborne <justin@eblah.com>
2022-08-18 12:16:34 -0500

    fixed bug with errors for channels

--- index.js
+++ index.js
@@ -533,7 +533,7 @@ class instance extends instance_skel {
 	 * @returns {Boolean}
 	 */
 	_is_valid_channel(id) {
-		if(id in this.channels && (this.channels[id].error ?? false) === false) {
+		if(id in this.channels && (this.channels[id].error?.message ?? false) === false) {
 			return true;
 		}
 
